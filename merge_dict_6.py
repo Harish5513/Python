81065197addb0ab1ec7268aafb18968977ea42d8
Harish5513 <96466381+Harish5513@users.noreply.github.com>
2021-12-23 15:58:57 +0530

    Delete merge_dict_6.py

--- merge_dict_6.py
+++
@@ -1,8 +0,0 @@
-def dict_mer(dict1,dict2):
-    return (dict2.update(dict1))
-dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
-dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
-print(dict1)
-print(dict2)
-dict_mer(dict1,dict2)
-print(dict2)
